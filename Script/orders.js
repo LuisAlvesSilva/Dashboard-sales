2bb665c4c55f223411593ea14d9a10fc2586357c
LuisAlvesSilva <contato.alvesluis@gmail.com>
2024-11-08 16:49:52 -0300

    second commit

---
+++ Script/orders.js
@@ -0,0 +1,21 @@
+const Orders = [
+    {
+        productName: 'JavaScript Tutorial',
+        productNumber: 'yyyyy',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: 'xxxxx',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        productNumber: 'zzzzz',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+];
+
